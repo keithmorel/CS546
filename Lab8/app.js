504330d6aa6e0a555d0e61ef366fd893cb546a74
keithmorel <kmorel1339@gmail.com>
2018-03-21 21:30:16 -0400

    First commit

---
+++ Lab8/app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const configRoutes = require("./routes");
+
+app.use(bodyParser.json());
+configRoutes(app);
+
+app.listen(3000, () => {
+  console.log("Routes will be running on http://localhost:3000");
+});
