321c040094427e9f85c8253f37653eb578e53444
keithmorel <kmorel1339@gmail.com>
2018-04-06 22:10:15 -0400

    Working

--- Lab9/public/js/form.js
+++ Lab9/public/js/form.js
@@ -1,30 +1,46 @@
-import { stat } from "fs";
-
 (function () {
 
-    function check(phrase) {
+    const palindromeMethods = {
+        check(phrase) {
             // Error checking
+            console.log(phrase);
             if (typeof phrase !== "string") throw "Input must be a string!";
             if (phrase == '') throw "Must provide a string to check!";
 
             // Palindrome check
-            let no_punc = phrase.toLowerCase().replace(/[^a-z]/g, '').replace(/\s+/g, '').trim();
+            let no_punc = phrase.toLowerCase().replace(/[^0-9a-z]/g, '').replace(/\s+/g, '').trim();
             if (no_punc == no_punc.split('').reverse().join('')) {
                 return true;
             }
             else {
                 return false;
             }
+        }
     };
+
+    function runPalindromeCheck() {
+        const returnFunction = palindromeMethods['check'];
+        return returnFunction;
+    }
     
-    const staticForm = document.getElementById("static-form");
+        const form = document.getElementById("palindrome-form");
 
-    if (staticForm) {
-        // Store all elements
-        const phrase = document.getElementById("phrase");
+        if (form) {
+            form.addEventListener("submit", event => {
+                event.preventDefault();
+                const phrase = document.getElementById("phrase").value;
+                const runPalindromeCheckFunc = runPalindromeCheck();
+                const palindromeCheck = runPalindromeCheckFunc(phrase);
 
-        staticForm.addEventListener("submit", event => {
-            event.preventDefault();
-        });
+                let li = document.createElement("li");
+                let phraseVal = document.createTextNode(phrase);
+                li.appendChild(phraseVal);
+
+                if (palindromeCheck) li.setAttribute("class", "is-palindrome");
+                else li.setAttribute("class", "not-palindrome");
+
+                document.getElementById("attempts").appendChild(li);
+                document.getElementById("phrase").value = '';
+            });
     }
 })();
--- Lab9/routes/palindrome.js
+++ Lab9/routes/palindrome.js
@@ -1,19 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const data = require("../data");
-const palindromeChecker = data.palindromeChecker;
+// const data = require("../data");
+// const palindromeChecker = data.palindromeChecker;
 
 router.get("/", (req, res) => {
     res.render("templates/server", {});
-});
-
-router.post("/", (req, res) => {
-    let phrase = req.body.phrase;
-    const is_palindrome = palindromeChecker.check(phrase);
-    res.render("templates/server", {
-        phrase: phrase,
-        palindromeCheck: is_palindrome
-    });
 });
 
 module.exports = router;
